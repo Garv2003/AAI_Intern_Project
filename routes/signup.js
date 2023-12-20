98078b2c04a13eabb2e8a9407206b2eede8c320c
Garv05 <coolgarv9@gmail.com>
2023-12-20 16:44:18 +0530

    changes

--- routes/signup.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const express = require("express");
-const router = express.Router();
-const routersignup=require("../controller/signup");
-
-router.get("/", routersignup.getlogin);
-router.post("/",routersignup.postlogin);
-
-module.exports = router;
